45dcc94cc567d6f9e72d2f9110343303f3c9c918
PyroRSC <109332014+PyroRSC@users.noreply.github.com>
2026-09-14 18:08:54 +0800

    added dbus update

--- start-dwm.sh
+++ start-dwm.sh
@@ -1,8 +1,11 @@
+xrdb -merge ~/.XResources
+xrandr --output DP-3 --primary --auto --output HDMI-0 --auto --left-of DP-3
 flameshot &
 nitrogen --restore &
 picom &
 dwmblocks &
 on_start.sh &
-x11vnc -display :0 -auth guess -forever -shared -bg -usepw -noxdamage -xkb
-[ -f ~/.Xresources ] && xrdb -merge ~/.Xresources
+#x11vnc -display :0 -auth guess -forever -shared -bg -usepw -noxdamage -xkb
+dbus-update-activation-environment --systemd DISPLAY XAUTHORITY
+
 exec dwm
